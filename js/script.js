ddf3f0a8f78bdef5940350e8225d7ee058231830
Christine Treacy <christine927t@gmail.com>
2020-01-08 16:52:37 -0500

    wrote functions to create gallery cards and modals

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
 
-//create gallery cards and append them to the DOM
-const $card = $("<div class='card'></div>")
-const $cardImg = $("<div class='card-img-container'></div>")
-const $cardInfo = $("<div class='card-info-container'></div>")
-const $img = $("<img class='card-img' src='https://placehold.it/90x90' alt='Profile picture'>")
-const $h3 = $("<h3 id='name' class='card-name cap'>First Last</h3>")
-const $email = $("<p class='card-text'>email</p>")
-const $cityState = $("<p class='card-text cap'>city, State</p>")
-
-$cardInfo.append($h3).append($email).append($cityState);
-$cardImg.append($img);
-$card.append($cardImg).append($cardInfo);
-$('#gallery').append($card);
-
 
 $.ajax({
     url: 'https://randomuser.me/api/',
@@ -21,28 +7,52 @@ $.ajax({
       console.log(data);
     }
   });
+//result of API will call createCards
 
+//result of clicking on card will call createModal
+
+//create gallery cards
+const $card = $("<div class='card'></div>")
+const $cardImgCont = $("<div class='card-img-container'></div>")
+const $cardInfo = $("<div class='card-info-container'></div>")
+const $cardImg = $("<img class='card-img' src='https://placehold.it/90x90' alt='Profile picture'>")
+const $cardName = $("<h3 id='name' class='card-name cap'>First Last</h3>")
+const $cardEmail = $("<p class='card-text'>email</p>")
+const $cardCityState = $("<p class='card-text cap'>city, State</p>")
+
+//appends gallery cards to the page
+function createCards(){
+    $cardInfo.append($cardName).append($cardEmail).append($cardCityState);
+    $cardImgCont.append($cardImg);
+    $card.append($cardImgCont).append($cardInfo);
+    $('#gallery').append($card);
+}
 
 //create modal elements
+const $modalCont = $("<div class='modal-container'></div>");
+const $modal = $("<div class='modal'></div>");
+const $button = $("<button type='button' id='modal-close-btn' class='modal-class-btn'><strong>X</strong></button>")
+const $modalInfo = $("<div class='modal-info-container'></div>")
+const $modalImg = $("<img class='modal-img' src='https://placehold.it/125x125' alt='Profile picture'>")
+const $modalName = $("<h3 id='name' class='modal-name cap'>First Last</h3>")
+const $modalEmail = $("<p class='modal-text'>email</p>")
+const $modalCity = $("<p class='modal-text cap'>City</p>")
+const $modalPhone = $("<p class='modal-text'>(555) 555-1212</p>")
+const $modalAddr = $("<p class='modal-text'>123 Portland Ave., Portland, OR 97204</p>")
+const $modalBday = $("<p class='modal-text'>Birthday: 10/21/2015</p>")
+        
+//appends modal elements to the page
+function createModal(){
+    $modalInfo.append($modalImg).append($modalName).append($modalEmail).append($modalCity).append("<hr>").append($modalPhone).append($modalAddr).append($modalBday);
+    $modal.append($button).append($modalInfo);
+    $modalCont.append($modal);
+    $('#gallery').append($modalCont)
+}
 
-/* <div class="modal-container">
-<div class="modal">
-    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-    <div class="modal-info-container">
-        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-        <h3 id="name" class="modal-name cap">name</h3>
-        <p class="modal-text">email</p>
-        <p class="modal-text cap">city</p>
-        <hr>
-        <p class="modal-text">(555) 555-5555</p>
-        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-        <p class="modal-text">Birthday: 10/21/2015</p>
-    </div>
-</div>
+$button.click(function(){
+    $modalCont.hide();  
+})
 
-// IMPORTANT: Below is only for exceeds tasks 
-<div class="modal-btn-container">
-    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-    <button type="button" id="modal-next" class="modal-next btn">Next</button>
-</div>
-</div> */
+createCards()
+createModal()
+
