a11de564f541b2bd9d856b110e5f7fe2b0edabe5
Christine Treacy <christine927t@gmail.com>
2020-01-09 16:53:47 -0500

    some changes, nothing helpful

--- js/script.js
+++ js/script.js
@@ -11,8 +11,6 @@ const $cardCityState = $("<p class='card-text cap'>city, State</p>")
 //appends gallery cards to the page
 function createCards(){
     $cardInfo.append($cardName).append($cardEmail).append($cardCityState);
-    //$cardInfo.append($cardName).append($cardEmail).append($cardCityState.addClass('card'));
-   // $cardInfo.append($cardName).append($cardEmail).append($cardCityState.addClass('card'));
     $cardImgCont.append($cardImg);
     $card.append($cardImgCont).append($cardInfo);
     $('#gallery').append($card);
@@ -26,44 +24,65 @@ const $modalInfo = $("<div class='modal-info-container'></div>")
 const $modalImg = $("<img class='modal-img' src='https://placehold.it/125x125' alt='Profile picture'>")
 const $modalName = $("<h3 id='name' class='modal-name cap'>First Last</h3>")
 const $modalEmail = $("<p class='modal-text'>email</p>")
-const $modalCity = $("<p class='modal-text cap'>City</p>")
+const $modalCityState = $("<p class='modal-text cap'>City</p>")
 const $modalPhone = $("<p class='modal-text'>(555) 555-1212</p>")
 const $modalAddr = $("<p class='modal-text'>123 Portland Ave., Portland, OR 97204</p>")
 const $modalBday = $("<p class='modal-text'>Birthday: 10/21/2015</p>")
         
 //appends modal elements to the page
 function createModal(){
-    $modalInfo.append($modalImg).append($modalName).append($modalEmail).append($modalCity).append("<hr>").append($modalPhone).append($modalAddr).append($modalBday);
+    $modalInfo.append($modalImg)
+      .append($modalName)
+      .append($modalEmail)
+      .append($modalCityState)
+      .append("<hr>")
+      //console.log("HR is appended")
+      .append($modalPhone)
+      .append($modalAddr)
+      .append($modalBday);
     $modal.append($button).append($modalInfo);
     $modalCont.append($modal);
     $('#gallery').append($modalCont)
 }
 
-$button.click(function(){
-    $modalCont.hide();  
-})
+const url = 'https://randomuser.me/api/?results=12&nat=us'
 
-const url = 'https://randomuser.me/api/?results=12'
+let $people;
+function generateCardHTML(data){
+    console.log(data)
+    $people = data.results;
+    console.log($people)
+    return $people.map(function(person){
+      createCards()
+      $cardImg.attr("src", `${person.picture.medium}`)
+      $cardName.text(`${person.name.first} ${person.name.last}`)
+      $cardEmail.text(`${person.email}`)
+      $cardCityState.text(`${person.location.city}`+`, `+ `${person.location.state}`)
+
+      //click handler to create modal
+      $card.click(function(){
+          createModal()
+          $modalImg.attr("src", `${person.picture.medium}`)
+          $modalName.text(`${person.name.first} ${person.name.last}`)
+          console.log($modalName.text())
+          $modalEmail.text(`${person.email}`)
+          $modalCityState.text(`${person.location.city}`)
+          $modalPhone.text(`${person.phone}`);
+          $modalAddr.text(`${person.location.street} ${person.location.state} ${person.location.postcode}`)
+      })
+    })
+  }
+
+
 
 /////////FETCH API REQUEST/////////
 fetch(url)
     .then((response) => response.json())
-    .then(function(data) {
-      console.log(data)
-      let $people = data.results;
-      console.log($people)
-      return $people.map(function(person){
-        createCards()
-        $cardImg.attr("src", `${person.picture.medium}`)
-        $cardName.text(`${person.name.first} ${person.name.last}`)
-        console.log($cardName.text())
-        $cardEmail.text(`${person.email}`)
-        $cardCityState.text(`${person.location.city}`+`, `+ `${person.location.state}`)
-      })
-    })
+    .then(generateCardHTML)
     .catch(function(error){
       console.log(error)      
     })
 
-
-  
+$button.click(function(){
+    $modalCont.hide();  
+})
