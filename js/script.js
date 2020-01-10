373709a43896fde7c80b598afc87c7c17e0ada8b
Christine Treacy <christine927t@gmail.com>
2020-01-10 16:48:44 -0500

    started search feature and CSS tweaking

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 let person;
 const url = 'https://randomuser.me/api/?results=12&nat=us'
 
+/////////SEARCH DIV FEATURE/////////
+//creates search elements
+const $searchForm = $("<form action='#' method='get'>")
+const $searchInput = $("<input type='search' id='search-input' class='search-input' placeholder='Search...'>")
+const $searchSubmit = $("<input type='submit' value='&#x1F50D;' id='search-submit class='search-submit'>")
+
+
+
 /////////HELPER FUNCTIONS/////////
-function createCards(person){
+const createCards = (person) => {
   //create gallery cards
   const $card = $("<div class='card'></div>")
   const $cardImgCont = $("<div class='card-img-container'></div>")
@@ -30,7 +38,7 @@ function createCards(person){
   })
 }
         
-function createModal(person){
+const createModal = (person) => {
   //create modal elements
   const $button = $("<button type='button' id='modal-close-btn' class='modal-class-btn'><strong>X</strong></button>")
   const $modalCont = $("<div class='modal-container'></div>");
@@ -43,6 +51,11 @@ function createModal(person){
   const $modalPhone = $("<p class='modal-text'>(555) 555-1212</p>")
   const $modalAddr = $("<p class='modal-text'>123 Portland Ave., Portland, OR 97204</p>")
   const $modalBday = $("<p class='modal-text'>Birthday: 10/21/2015</p>")
+  const $bdaySlice = `${person.dob.date}`
+  const $bdayNew = ($bdaySlice.substr(5,2) +'/'+ $bdaySlice.substr(8,2) +'/'+ $bdaySlice.substr(2,2))  
+  const $btnContainer = $("<div class='modal-btn-container'>")
+  const $nextBtn = $("<button type='button' id='modal-prev' class='modal-prev btn'>Prev</button>")
+  const $prevBtn = $("<button type='button' id='modal-next' class='modal-next btn'>Next</button>")
 
   //appends modal elements to the page
   $modalInfo.append($modalImg)
@@ -52,19 +65,20 @@ function createModal(person){
     .append("<hr>")
     .append($modalPhone)
     .append($modalAddr)
-    .append($modalBday);
-  $modal.append($button).append($modalInfo);
+    .append($modalBday)
+  $btnContainer.append($prevBtn).append($nextBtn)
+  $modal.append($button).append($modalInfo).append($btnContainer);
   $modalCont.append($modal);
   $('#gallery').append($modalCont)
 
   //interpolating person info for each modal
   $modalImg.attr("src", `${person.picture.large}`)
   $modalName.text(`${person.name.first} ${person.name.last}`)
-  console.log($modalName.text())
   $modalEmail.text(`${person.email}`)
   $modalCityState.text(`${person.location.city}`)
   $modalPhone.text(`${person.phone}`);
-  $modalAddr.text(`${person.location.street} ${person.location.state} ${person.location.postcode}`)
+  $modalAddr.text(`${person.location.street.number} ${person.location.street.name}`+`, `+ `${person.location.state} ${person.location.postcode}`)
+  $modalBday.text(`Birthday: `+ $bdayNew)
 
   //click handler to for the 'close' button on modal
   $button.click(function(){
@@ -72,11 +86,39 @@ function createModal(person){
   })
 }
 
+//appends search elements to the page
+$searchForm.append($searchInput).append($searchSubmit)
+$('.search-container').append($searchForm)
+
+const $search = (text, people) => {
+  let $searchStore = []
+  people.map(function(person){
+    let $fullName = (`${person.name.first} ${person.name.last}`)
+    if ($fullName.toLowerCase().includes(text.toLowerCase())){
+      $searchStore.push(person);
+    }  
+  })
+
+  console.log($searchStore)
+  if ($searchStore.length > 0) {
+    $('.card').hide();
+    //debugger;
+    console.log(people)
+    console.log($searchStore)
+    createCards($searchStore)
+  }
+}
+
+$searchSubmit.click(function(){
+  event.preventDefault();
+  text = $searchInput.val();
+  $search(text,people)
+})
+
 //generates HTML for each card
-function generateCardHTML(data){
+const generateCardHTML = (data) => {
     console.log(data)
     people = data.results;
-    console.log(people)
     return people.map(function(person){
       createCards(person)
       //debugger;
@@ -90,4 +132,5 @@ fetch(url)
     .catch(function(error){
       console.log(error)      
     })
+
 
